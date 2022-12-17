578749b0a21961f251c3860ace6b1bb76de6edbb
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-12-17 13:57:39 +0100

    Create SimpleMultiplication.py

---
+++ SimpleMultiplication.py
@@ -0,0 +1,24 @@
+import random
+
+def ask_question():
+  # Generate two random numbers to multiply
+  num1 = random.randint(1, 10)
+  num2 = random.randint(1, 10)
+
+  # Ask the user to solve the multiplication problem
+  answer = int(input(f"What is {num1} * {num2}? "))
+
+  # Check if the answer is correct
+  if answer == num1 * num2:
+    print("Correct!")
+  else:
+    print("Incorrect. The correct answer is", num1 * num2)
+
+# Ask the user five questions
+for i in range(5):
+  ask_question()
+
+
+#The Git part since I still need to improve my skills in it. 
+
+  #git remote add origin 
