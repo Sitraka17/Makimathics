32e861a960c189f1698fd6d95ca4b6fa5b1901c2
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-03 16:26:31 +0100

    Delete VAR.py

--- Finance&Maths/VAR.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-#%matplotlib inline
-
-money_df = pd.read_csv('M2SLMoneyStock.csv')
-spending_df = pd.read_csv('PCEPersonalSpending.csv')
-df = money_df.join(spending_df)
