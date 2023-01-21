089703a88d758d0ea92b47179fedd6903fd3d97d
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-22 00:02:25 +0100

    Create Fibonacci.py

---
+++ Suites/Fibonacci.py
@@ -0,0 +1,19 @@
+#En l'honneur de la nouvelle année 2023 qui est l'année du Lapin ! 
+#voici la suite de Fibonacci 
+def fibonacci(n):
+    a = 0
+    b = 1
+    if n == 1:
+        print(a)
+    elif n == 2:
+        print(a, b)
+    else:
+        print(a, b, end=" ")
+        for i in range(n-2):
+            c = a + b
+            a = b
+            b = c
+            print(b, end=" ")
+
+n = int(input("Enter the number of terms: "))
+fibonacci(n)
